02961383aabab2a3cc09c9bbe9c254399a33be0b
Mayank-MP05 <mayank5pande@gmail.com>
2021-02-27 12:50:38 +0530

    Router Routes And Specificationa dn connecting to main

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect, Fragment } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
+function Routes() {
+  return (
+    <Switch>
+      <Route path='/dashboard'>
+
+      </Route>
+    </Switch>
+  );
+}
+
+export default Routes;
