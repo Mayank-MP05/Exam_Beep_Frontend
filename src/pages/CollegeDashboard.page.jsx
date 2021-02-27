1cb5e7c0200ee711ad90c29763a41aa92f33fb72
Mayank-MP05 <mayank5pande@gmail.com>
2021-02-27 15:09:25 +0530

    Dummy Content for Collge Dashboard

--- src/pages/CollegeDashboard.page.jsx
+++ src/pages/CollegeDashboard.page.jsx
@@ -1,7 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+  Card,
+  Button,
+  CardTitle,
+  CardText,
+  Row,
+  Col,
+} from "reactstrap";
+import classnames from "classnames";
 
 function CollegeDashboard() {
-  return <div>This is College Dashboard</div>;
+  const [activeTab, setActiveTab] = useState("1");
+
+  const toggle = (tab) => {
+    if (activeTab !== tab) setActiveTab(tab);
+  };
+
+  return (
+    <div>
+      <Nav tabs>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === "1" })}
+            onClick={() => {
+              toggle("1");
+            }}>
+            Studentts
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === "2" })}
+            onClick={() => {
+              toggle("2");
+            }}>
+            Exams
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === "3" })}
+            onClick={() => {
+              toggle("3");
+            }}>
+            Results
+          </NavLink>
+        </NavItem>
+      </Nav>
+      <TabContent activeTab={activeTab}>
+        <TabPane tabId='1'>
+          <Row>
+            <Col sm='12'>
+              <h4>Student table remdered here</h4>
+            </Col>
+          </Row>
+        </TabPane>
+        <TabPane tabId='2'>
+          <Row>
+            <Col sm='12'>
+              <h4>Exam table rendered here</h4>
+            </Col>
+          </Row>
+        </TabPane>
+        <TabPane tabId='3'>
+          <Row>
+            <Col sm='12'>
+              <h4>Result table remdered here</h4>
+            </Col>
+          </Row>
+        </TabPane>
+      </TabContent>
+    </div>
+  );
 }
 
 export default CollegeDashboard;
