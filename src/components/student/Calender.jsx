b672444b92cf7938404a2ecc40c574c2ffc779cd
Mayank-MP05 <mayank5pande@gmail.com>
2021-02-27 22:12:56 +0530

    College Signup Size

--- src/components/student/Calender.jsx
+++ src/components/student/Calender.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
 
-function Calender() {
-  return <div>Calender Component</div>;
+export default class Calender extends React.Component {
+  render() {
+    return (
+      <FullCalendar plugins={[dayGridPlugin]} initialView='dayGridMonth' />
+    );
+  }
 }
-
-export default Calender;
