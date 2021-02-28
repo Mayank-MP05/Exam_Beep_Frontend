aa6eef5d3051ff02e22f9c2486be8c7096b7285c
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2021-02-28 05:51:33 +0530

    added Calender V2

--- src/components/student/Calender.jsx
+++ src/components/student/Calender.jsx
@@ -1,9 +1,50 @@
 import React from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import "@fullcalendar/core";
+import "@fullcalendar/daygrid";
+import "@fullcalendar/timegrid";
+import "../../styles/Calender.css";
 
-export default class Calender extends React.Component {
-  render() {
-    return "<FullCalendar plugins={[dayGridPlugin]} initialView='dayGridMonth' />";
-  }
+let newEvent = {
+  title: "New event",
+  date: new Date(),
+  description: "A new event",
+  link:
+    "https://docs.google.com/forms/d/e/1FAIpQLScPNBVVbKRQL_Rfxi9AI2oeJGzGExhqFjxCmuFiexTouj_m2Q/viewform?fbzx=-174935308807220936",
+};
+const events = [newEvent];
+
+export default function ReactCalender() {
+  return (
+    <div className="container">
+      <FullCalendar
+        aspectRatio={1.75}
+        initialView="dayGridMonth"
+        headerToolbar={{
+          left: "prev,next",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay",
+        }}
+        events={events}
+        plugins={[dayGridPlugin, timeGridPlugin]}
+      />
+      {events.map((exam, index) => (
+        <div className="card upcomingExam" key={index}>
+          <div class="card-body content">
+            <div>
+              <h3 class="card-title">{exam.title}</h3>
+              <p class="card-subtitle text-muted">
+                {exam.date.toDateString()} {exam.date.toLocaleTimeString()}
+              </p>
+            </div>
+            <a href={exam.link} class="btn btn-primary">
+              Exam Link
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
